bbf3ab810e4dced9ae0098c752a22e9696e602cd
Ömer <omerkeskin980@gmail.com>
2025-03-27 14:29:54 +0300

    security check commit2

--- main.go
+++ main.go
@@ -25,6 +25,9 @@ type apiConfig struct {
 var staticFiles embed.FS
 
 func main() {
+	file := os.Getenv("UNSAFE_PATH")
+    data, _ := os.ReadFile(file)
+    _ = data
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
